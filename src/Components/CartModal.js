b0f17662cbbec24cd88e02a49dce0715e62d9510
Dev Mello <caiuabrandao@gmail.com>
2024-05-17 08:03:49 -0300

    layoult changes

---
+++ src/Components/CartModal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './components-styles/CartModal.css'
+
+export default function CartModal({items}){
+    return (
+        <div className="CartModal">
+
+        </div>
+    )
+}
