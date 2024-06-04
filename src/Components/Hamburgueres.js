b8460782a425f750ad412d1a3b44c12db4a5a2c6
Dev Mello <caiuabrandao@gmail.com>
2024-06-04 10:58:18 -0300

    update done

--- src/Components/Hamburgueres.js
+++ src/Components/Hamburgueres.js
@@ -9,14 +9,10 @@ export default function Hamburgueres({setCartItems, cardapio, setModalGeralShow,
 
     return (
         <div className='Hamburgueres'>
-
-
             <HomeSection>
                 <h1 className='box-title'>HAMBURGUERES</h1>
                 <Cards setModalGeralShow={setModalGeralShow} setModalGeralText={setModalGeralText} cardapio={cardapio_hamburguer} setCartItems={setCartItems} />
             </HomeSection>
-
-
         </div>
     )
 }
